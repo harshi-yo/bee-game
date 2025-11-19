04f063d112020c9afc89ab5f43d50f5d91683221
pinnamarajusriharshita-hash <pinnamarajusriharshita@gmail.com>
2025-11-20 01:07:49 +0530

    Initial Bee Game project commit

--- bee_game/urls.py
+++ bee_game/urls.py
@@ -1,6 +1,7 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('game/', views.game_view, name='game'),
 ]
--- bee_game/views.py
+++ bee_game/views.py
@@ -1,4 +1,9 @@
+
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("Welcome to the Bee Game!")
+
+def game_view(request):
+    return render(request, 'bee_game/game.html')
